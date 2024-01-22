52ce3b5c9c149bdc844282a0a21656e833f49d49
Oleksandr Hereha <gerega.2007@gmail.com>
2024-01-22 12:33:04 +0100

    version 1

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Spa Centre</title>
+</head>
+<body>
+    <header class="main-header">
+    
+    <div class="topbar">
+        <div class="contact">
+            <span><ion-icon name="call-outline"></ion-icon> +012 345 789</span>
+            <span>&#124;</span>
+            <span><ion-icon name="mail-outline"></ion-icon> info@example.com</span>
+        </div>
+        <div class="social-net">
+            <span>f</span>
+            <span>x</span>
+            <span>l</span>
+            <span>i</span>
+            <span>y</span>
+        </div>
+    </div>
+
+    <nav class="navbar">
+        <div>
+            <span class="logo-spa logo-text">SPA</span>
+            <span class="logo-center logo-text">Center</span>
+        </div>
+        <div class="main-menu-bar">
+            <ul class="main-menu-list">
+                <li class="main-menu-item"><a href="#" class="main-menu-link">Home</a></li>
+                <li class="main-menu-item"><a href="#" class="main-menu-link">About</a></li>
+                <li class="main-menu-item"><a href="#" class="main-menu-link">Services</a></li>
+                <li class="main-menu-item"><a href="#" class="main-menu-link">Pricing</a></li>
+                <li class="main-menu-item"><a href="#" class="main-menu-link">Pages</a></li>
+                <li class="main-menu-item"><a href="#" class="main-menu-link">Contact</a></li>
+            </ul>
+        </div>
+        <div class="button-appointment">
+            <a href="#">Make Appointment</a>
+        </div>
+    </nav>
+
+    </header>
+
+    <div id="hero-section">
+        <div>
+            <p>text</p>
+            <h2>Treatment</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat soluta necessitatibus eum placeat nostrum sint, cupiditate tempora consequatur, laboriosam accusantium voluptas harum, reiciendis obcaecati animi voluptates praesentium non incidunt.</p>
+            <a href="">Make appointment</a>
+        </div>
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
